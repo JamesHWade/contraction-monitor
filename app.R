27338cc9b371ecc4d8749666b910b61a87e9536d
James Wade <github@jameshwade.com>
2023-09-16 09:32:10 -0400

    add offline auth code

--- app.R
+++ app.R
@@ -11,6 +11,24 @@ options(gargle_oauth_cache = ".secrets")
 gs4_auth(cache = ".secrets", email = "james.wade1221@gmail.com")
 contractions <- gs4_get(url)
 
+# Uncomment to setup offline auth
+# # Set authentication token to be stored in a folder called `.secrets`
+# options(gargle_oauth_cache = ".secrets")
+#
+#
+# # Authenticate manually
+# gs4_auth()
+#
+# # If successful, the previous step stores a token file.
+# # Check that a file has been created with:
+# list.files(".secrets/")
+#
+# # Check that the non-interactive authentication works by first deauthorizing:
+# gs4_deauth()
+#
+# # Authenticate using token. If no browser opens, the authentication works.
+# gs4_auth(cache = ".secrets", email = "james.wade1221@gmail.com")
+
 ui <- page_fluid(
   theme = bs_theme(version = 5, bootswatch = "minty"),
   card(
